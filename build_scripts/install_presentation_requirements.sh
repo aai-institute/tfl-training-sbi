9b5c17b7d2cab8dccd684583696ef41d9f9edf96
turnmanh <17703667+turnmanh@users.noreply.github.com>
2024-01-25 19:52:59 +0100

    pinned jupyter notebook and rise to v6, fixing #30

--- build_scripts/install_presentation_requirements.sh
+++ build_scripts/install_presentation_requirements.sh
@@ -38,7 +38,8 @@ BUILD_DIR=$(dirname "$0")
 (
   cd "${BUILD_DIR}/.." || (echo "Unknown error, could not find directory ${BUILD_DIR}" && exit 255)
   conda install -y -c conda-forge notebook==6.5.6 jupyter jupyter_contrib_nbextensions jupyter_nbextensions_configurator
-  conda install -y anaconda::rise==5.7.1
+  # conda install -y -c anaconda rise==5.7.1
+  pip install rise
   python build_scripts/configure_spellcheck_dict.py
   jupyter contrib nbextension install --user
   jupyter nbextensions_configurator enable --user
